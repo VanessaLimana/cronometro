d0ee2812415b48d81013d995788f43043cecd656
Vanessa Limana <vanessa4562008@hotmail.com>
2021-03-02 20:43:01 -0300

    fim cronometro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
-import './style.css'
+import './style.css';
 
 class App extends Component{
 
   constructor(props){
     super(props);
     this.state = {
-      numero: 0
+      numero: 0,
+      botao: 'VAI'
     };
 
     this.vai = this.vai.bind(this);
@@ -14,28 +15,43 @@ class App extends Component{
     this.timer = null;
   }
   vai(){
+    let state = this.state;
+
     if(this.timer !== null){
-      
-    }
+      clearInterval(this.timer);
+      this.timer = null;
+      this.state.botao = 'VAI';
+
+    }else{
     this.timer = setInterval(()=>{
       let state = this.state;
       state.numero += 0.1;
       this.setState(state);
-    },100)
+    },100);
+    state.botao = 'PAUSAR';
+      }
+    this.setState(state);
 
-  }
+    }
   limpar(){
-
-
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+      this.state.botao = 'VAI';
+    }
+    let state = this.state;
+    state.numero =0;
+    state.botao = 'VAI';
+    this.setState(state);
   }
   render(){
     return(
       <div className="container">
-        <img src={require('.assets/cronometro.png')} />
+        <img src={require('./assets/cronometro.png')} />
         <a className="timer">{this.state.numero.toFixed(1)}0.0</a>
         <div className="areaBtn">
-          <a className="botao" onClick={this.vai}> VAI</a>
-          <a className="botao"> onClick={this.limpar}LIMPAR</a>
+          <a className="botao" onClick={this.vai}> {this.state.botao}</a>
+          <a className="botao" onClick={this.limpar}>LIMPAR</a>
         </div>
       </div>
         );
